eb0cd5b76667c65f9b996c0462fda7a91ea8c56a
N.K <naumankhan642@gmail.com>
2023-10-27 02:15:35 +0500

    Added show page and style

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,4 +3,8 @@ class PublicController < ApplicationController
     @q = User.ransack(params[:q])
     @users = @q.result.order(:created_at)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'public#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'public/show/:id', to: 'public#show', as: 'public_show'
 end
